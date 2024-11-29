a12a4ac081f1d9f19829cecd71224ba6a0d93052
AlissonRodrigues <rodrigues101112@gmail.com>
2024-11-29 18:48:34 -0300

    update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,76 @@
-import { useState } from "react";
-
-import viteLogo from "/vite.svg";
-import "./blocks/app.css";
+import "./App.jsx";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank"></a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <header className="header">
+        <nav className="nav">
+          <img
+            src="<%= require('./images/logo_around.png')%>"
+            alt="imagem do logo da page"
+          />
+        </nav>
+      </header>
+      <section className="profile">
+        <div className="profile-edit-avt">
+          <img
+            src="<%= require('./images/image_header.jpg')%>"
+            alt=" imagem do perfil"
+            className="profile__image"
+          />
+          <button alt="Editar avatar" className="profile__edit-photo"></button>
+        </div>
+        <div className="profile__card">
+          <h2 className="profile__name">Jacques Cousteau</h2>
+          <div>
+            <button className="profile__button">
+              <img
+                src="<%= require('./images/profile__botton.png')%>"
+                alt="botao"
+                id="#login"
+              />
+            </button>
+          </div>
+          <p className="profile__description">Explorador</p>
+        </div>
+
+        <button className="profile__add">
+          <img
+            src="<%= require('./images/profile__add.png')%>"
+            alt="adicionar"
+            className="profile__add-bt"
+          />
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      </section>
+      <main className="cards">
+        <template id="card-template">
+          <div className="cards__container">
+            <button className="cards__button-remove">
+              <img src="" alt="" className="cards__image" />
+            </button>
+            <img
+              id="remove-image"
+              src="<%= require('./images/Trash.png')%>"
+              alt="delete"
+              className="cards__delete"
+            />
+            <div className="cards__container-title">
+              <h3 className="cards__container-name"></h3>
+              <button className="cards__button" type="button">
+                <img
+                  className="cards__button-like"
+                  src="<%= require('./images/image__like.png')%>"
+                  alt="icone de like"
+                />
+                <span className="cards__like-counter">0</span>
+              </button>
+            </div>
+          </div>
+        </template>
+      </main>
+      <footer className="footer">
+        <p>© 2024 Around The U.S.</p>
+      </footer>
     </>
   );
 }
