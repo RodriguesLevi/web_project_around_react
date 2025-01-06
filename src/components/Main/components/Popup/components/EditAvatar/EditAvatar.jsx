8de1785d696708e852adc5f1fec24f938e61e657
Alisson <rodrigues101112@gmail.com>
2025-01-06 10:46:36 -0300

    update file editavatar.jsx

--- src/components/Main/components/Popup/components/EditAvatar/EditAvatar.jsx
+++ src/components/Main/components/Popup/components/EditAvatar/EditAvatar.jsx
@@ -1,6 +1,27 @@
+import { useContext, useState } from "react";
+import { CurrentUserContext } from "../../../../../../contexts/CurrentUserContext";
+
 export default function EditAvatar() {
+  const { currentUser, handleUpdateAvatar } = useContext(CurrentUserContext);
+  const [avatar, setAvatar] = useState(currentUser.avatar);
+
+  const handleAvatarChange = (event) => {
+    setAvatar(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    handleUpdateAvatar({ avatar });
+  };
+
   return (
-    <form id="avatar-form" className="form form_edit-avatar" name="edit-avatar">
+    <form
+      id="avatar-form"
+      className="form form_edit-avatar"
+      name="edit-avatar"
+      onSubmit={handleSubmit}
+    >
       <fieldset className="form__fieldset">
         <input
           type="url"
@@ -9,6 +30,8 @@ export default function EditAvatar() {
           className="form__input form__input_el_link"
           placeholder="Link da imagem"
           required
+          value={avatar}
+          onChange={handleAvatarChange}
         />
         <button disabled type="submit" className="form__button">
           Salvar
